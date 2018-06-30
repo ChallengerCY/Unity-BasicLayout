bfd4e405a2787565b33d145bf379e7f358f21fea
ChallengerCY <790597004@qq.com>
2018-06-30 21:30:10 +0800

    modify

---
+++ TestRectTransform/Assets/TEST.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TEST : MonoBehaviour
+{
+
+
+    public RectTransform Image1, Image2;
+
+    // Use this for initialization
+    void Start()
+    {
+       // Debug.Log(Image2.anchoredPosition);
+
+    }
+
+    private void Update()
+    {
+       // Debug.Log(Image2.anc);
+    }
+}
